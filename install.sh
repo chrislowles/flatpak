8553542a9414d0468a2ca2921311889d1938d63f
Chris Lowles <lowleschris@gmail.com>
2025-07-27 03:11:45 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,46 +5,18 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-full() {
-	flatpak install flathub com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep me.timschneeberger.jdsp4linux org.gnome.Boxes com.obsproject.Studio org.audacityteam.Audacity org.kde.krita org.kde.kdenlive io.github.shiftey.Desktop org.qbittorrent.qBittorrent fr.handbrake.ghb org.musicbrainz.Picard dev.vencord.Vesktop us.zoom.Zoom com.sindresorhus.Caprine app.ytmdesktop.ytmdesktop io.freetubeapp.FreeTube io.github.pwr_solaar.solaar org.upscayl.Upscayl \ com.valvesoftware.Steam com.steamdeckrepo.manager net.lutris.Lutris net.davidotek.pupgui2 io.github.limo_app.limo com.github.mtkennerly.ludusavi com.steamgriddb.steam-rom-manager com.modrinth.ModrinthApp uk.co.powdertoy.tpt org.azahar_emu.Azahar org.flycast.Flycast com.github.AmatCoder.mednaffe org.DolphinEmu.dolphin-emu io.github.simple64.simple64 ca._0ldsk00l.Nestopia org.ppsspp.PPSSPP org.duckstation.DuckStation net.pcsx2.PCSX2 net.rpcs3.RPCS3 net.shadps4.shadPS4 info.cemu.Cemu com.snes9x.Snes9x
-}
-
-basic() {
-	flatpak install flathub com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep me.timschneeberger.jdsp4linux org.gnome.Boxes com.obsproject.Studio org.audacityteam.Audacity org.kde.krita org.kde.kdenlive io.github.shiftey.Desktop org.qbittorrent.qBittorrent fr.handbrake.ghb org.musicbrainz.Picard dev.vencord.Vesktop us.zoom.Zoom com.sindresorhus.Caprine app.ytmdesktop.ytmdesktop io.freetubeapp.FreeTube io.github.pwr_solaar.solaar org.upscayl.Upscayl
-}
-
 doTheThing() {
-	. /etc/os-release
-	case "$ID" in
-		linuxmint)
-			apt update
-			apt install steam-devices
-			flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-			;;
-		fedora)
-			dnf update
-			dnf upgrade
-			dnf install steam-devices
-			flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-			flatpak install --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1)
-			flatpak remote-delete fedora
-			;;
-		*)
-			exit 1
-			;;
-	esac
 	while
-		read -p "fF for full base - bB for crucial/laptop base" base
+		read -p "Install preferred flatpaks (y/N)" yn
 	do
-		case $base in
-			([fF]) full;;
-			([bB]) basic;;
+		case $yn in
+			([yY]) flatpak install flathub org.audacityteam.Audacity com.github.huluti.Curtail io.github.ungoogled_software.ungoogled_chromium com.github.qarmin.czkawka io.freetubeapp.FreeTube io.github.ellie_commons.jorts org.kde.kdenlive org.kde.krita org.kde.kate org.libreoffice.LibreOffice com.modrinth.ModrinthApp org.nicotine_plus.Nicotine com.obsproject.Studio org.qbittorrent.qBittorrent dev.geopjr.Tuba org.upscayl.Upscayl dev.vencord.Vesktop app.ytmdesktop.ytmdesktop us.zoom.Zoom;;
 			(*) break;;
 		esac
 	done
 }
 
 if ! doTheThing; then
-	echo "Couldn't install base preferred flatpaks."
+	echo "Couldn't/Didn't install base preferred flatpaks."
 	exit 1
 fi
