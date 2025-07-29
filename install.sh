b385c4f59eb2023a7331f9477ef3beb10fe5c1c4
Chris Lowles <lowleschris@gmail.com>
2025-07-29 18:30:29 +1000

    added unofficial beeper flatpak

--- install.sh
+++ install.sh
@@ -6,17 +6,16 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 doTheThing() {
-	while
-		read -p "Install preferred flatpaks (y/N)" yn
-	do
-		case $yn in
-			([yY]) flatpak install flathub org.audacityteam.Audacity com.github.huluti.Curtail io.github.ungoogled_software.ungoogled_chromium com.github.qarmin.czkawka io.freetubeapp.FreeTube io.github.ellie_commons.jorts org.kde.kdenlive org.kde.krita org.kde.kate org.libreoffice.LibreOffice com.modrinth.ModrinthApp org.nicotine_plus.Nicotine com.obsproject.Studio org.qbittorrent.qBittorrent dev.geopjr.Tuba org.upscayl.Upscayl dev.vencord.Vesktop app.ytmdesktop.ytmdesktop us.zoom.Zoom;;
-			(*) break;;
-		esac
-	done
+	sudo flatpak remote-add --if-not-exists flatpaks-beeper https://flatpaks.github.io/beeper/beeper.flatpakrepo
+	sudo flatpak install flatpaks-beeper com.beeper.beeper
+	flatpak install flathub org.audacityteam.Audacity com.github.huluti.Curtail io.github.ungoogled_software.ungoogled_chromium com.github.qarmin.czkawka io.freetubeapp.FreeTube io.github.ellie_commons.jorts org.kde.kdenlive org.kde.krita org.kde.kate org.libreoffice.LibreOffice com.modrinth.ModrinthApp org.nicotine_plus.Nicotine com.obsproject.Studio org.qbittorrent.qBittorrent dev.geopjr.Tuba org.upscayl.Upscayl dev.vencord.Vesktop app.ytmdesktop.ytmdesktop us.zoom.Zoom
 }
 
-if ! doTheThing; then
-	echo "Couldn't/Didn't install base preferred flatpaks."
-	exit 1
-fi
+while
+	read -p "Install preferred flatpaks (y/N)" yn
+do
+	case $yn in
+		([yY]) doTheThing;;
+		(*) break;;
+	esac
+done
