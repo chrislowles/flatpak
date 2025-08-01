28d045278dd217ffbd9945b97015b2732ec2896e
Chris Lowles <lowleschris@gmail.com>
2025-08-02 04:26:32 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 doTheThing() {
 	#sudo flatpak remote-add --if-not-exists flatpaks-beeper https://flatpaks.github.io/beeper/beeper.flatpakrepo
 	#sudo flatpak install flatpaks-beeper com.beeper.beeper
-	flatpak install flathub org.audacityteam.Audacity com.github.huluti.Curtail io.github.ungoogled_software.ungoogled_chromium com.github.qarmin.czkawka io.freetubeapp.FreeTube io.github.ellie_commons.jorts org.kde.kdenlive org.kde.krita org.kde.kate org.libreoffice.LibreOffice com.modrinth.ModrinthApp org.nicotine_plus.Nicotine com.obsproject.Studio org.qbittorrent.qBittorrent dev.geopjr.Tuba org.upscayl.Upscayl dev.vencord.Vesktop app.ytmdesktop.ytmdesktop us.zoom.Zoom
+	flatpak install flathub org.audacityteam.Audacity com.github.huluti.Curtail io.github.ungoogled_software.ungoogled_chromium com.github.qarmin.czkawka io.freetubeapp.FreeTube io.github.ellie_commons.jorts org.kde.kdenlive org.kde.krita org.kde.kate org.libreoffice.LibreOffice com.modrinth.ModrinthApp org.nicotine_plus.Nicotine com.obsproject.Studio org.qbittorrent.qBittorrent dev.geopjr.Tuba org.upscayl.Upscayl dev.vencord.Vesktop app.ytmdesktop.ytmdesktop us.zoom.Zoom io.github.pwr_solaar.solaar
 }
 
 while
